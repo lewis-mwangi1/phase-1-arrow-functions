134cc0f8bb76e7e97c815e3029e4c3ad407224cc
Lewis <gluwiz@gmail.com>
2023-03-18 16:45:38 +0300

    Arrow Functions Lab Attempt

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide =  function(a, b) {    
+    return a / b;
+};
+
+console.log(divide(2000, 100));
+
+const square = (num) => {
+    return num * num;
+  };
+
+  const add = (a, b) => {
+  return a + b;
+};
